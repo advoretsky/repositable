8c7a68b84b68c8af8942b5bcb67a539e0cb30950
Paweł Płazieński <nivertius@gmail.com>
2016-10-29 12:10:03 +0200

    Add logging facility using logback

--- src/main/java/org/perfectable/artifactable/Server.java
+++ src/main/java/org/perfectable/artifactable/Server.java
@@ -8,6 +8,8 @@ import org.perfectable.webable.handler.HandlerServerConfigurationExtension;
 import org.perfectable.webable.handler.HttpResponse;
 import org.perfectable.webable.handler.RequestHandler;
 import org.perfectable.webable.handler.authorization.BasicAuthenticationRequestChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -15,6 +17,8 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public final class Server {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
 	private final int port;
 	private final Repositories repositories;
 	private final Group users;
@@ -56,11 +60,13 @@ public final class Server {
 	private static final int REQUIRED_ARGUMENTS_COUNT = 1;
 
 	public static void main(String[] args) throws JAXBException {
+		LOGGER.info("Starting server; Working directory is {}", System.getProperty("user.dir"));
 		if(args.length < REQUIRED_ARGUMENTS_COUNT) {
 			System.out.println("Usage: artifactable <configuration>"); // NOPMD actual use of system out
 			return;
 		}
 		String configurationLocation = args[0];
+		LOGGER.info("Reading configuration from {}", configurationLocation);
 		File configurationFile = new File(configurationLocation);
 		JAXBContext jaxbContext = JAXBContext.newInstance(ServerConfiguration.class);
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
