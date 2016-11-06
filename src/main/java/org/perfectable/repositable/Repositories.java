437456f01f3cfc912d1cdb717f54effe44b49e3b
Paweł Płazieński <nivertius@gmail.com>
2016-11-06 16:19:47 +0100

    Sort components in Repositories to have predictable and stable results

--- src/main/java/org/perfectable/repositable/Repositories.java
+++ src/main/java/org/perfectable/repositable/Repositories.java
@@ -1,6 +1,6 @@
 package org.perfectable.repositable;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
 import org.perfectable.repositable.metadata.Metadata;
 
 import java.util.Collection;
@@ -10,28 +10,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class Repositories implements RepositorySelector, RepositorySet {
-	private final ImmutableMap<String, Repository> repositoryByName;
+	private final ImmutableSortedMap<String, Repository> repositoryByName;
 
 	public static Repositories create() {
-		return new Repositories(ImmutableMap.of());
+		return new Repositories(ImmutableSortedMap.of());
 	}
 
-	private Repositories(ImmutableMap<String, Repository> repositoryByName) {
+	private Repositories(ImmutableSortedMap<String, Repository> repositoryByName) {
 		this.repositoryByName = repositoryByName;
 	}
 
 	public Repositories withAdditional(String name, Repository repository) {
-		ImmutableMap<String, Repository> newRepositoryByName = ImmutableMap.<String, Repository>builder()
-				.putAll(repositoryByName)
-				.put(name, repository)
-				.build();
+		ImmutableSortedMap<String, Repository> newRepositoryByName =
+				ImmutableSortedMap.<String, Repository>naturalOrder()
+						.putAll(repositoryByName)
+						.put(name, repository)
+						.build();
 		return new Repositories(newRepositoryByName);
 	}
 
 	@Override
 	public Repository select(String repositoryName) {
 		Repository repository = repositoryByName.get(repositoryName);
-		if(repository == null) {
+		if (repository == null) {
 			return EmptyRepository.INSTANCE;
 		}
 		return repository;
@@ -50,7 +51,7 @@ public final class Repositories implements RepositorySelector, RepositorySet {
 		return repositoryByName.values().stream()
 				.map(transformation)
 				.flatMap(candidate -> candidate.isPresent() ? Stream.of(candidate.get()) : Stream.empty())
-				.collect(Collectors.toSet());
+				.collect(Collectors.toList());
 	}
 
 }
